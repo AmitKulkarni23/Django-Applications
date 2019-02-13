70e2ec838e9a1208e986f6a8e178c5dbf5b579bf
amitrkulkarni232@gmail.com <amitrkulkarni232@gmail.com>
2019-02-12 20:09:30 -0500

    Commiting changes to models

--- TryDjango/src/products/models.py
+++ TryDjango/src/products/models.py
@@ -3,7 +3,13 @@ from django.db import models
 
 # Create your models here.
 class Product(models.Model):
-    title = models.TextField(default='Basketball')
-    description = models.TextField(default='This is my 1st model')
-    price = models.TextField(default='100')
-    summary = models.TextField(default='This is cool')
+    # Title field should be limited to how long it is
+    title = models.CharField(max_length=120)
+
+    description = models.TextField(blank=True, null=True)
+
+    # Decimal Field
+    price = models.DecimalField(decimal_places=2, max_digits=1000)
+
+    # Summary is a text field
+    summary = models.TextField()
