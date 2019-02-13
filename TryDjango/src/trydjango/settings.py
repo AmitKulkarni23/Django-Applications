d1fb7717d27dac17e7893c55fc37e42728e607bb
amitrkulkarni232@gmail.com <amitrkulkarni232@gmail.com>
2019-02-12 19:49:20 -0500

    Try Django - Created 1st ever model

--- TryDjango/src/trydjango/settings.py
+++ TryDjango/src/trydjango/settings.py
@@ -39,6 +39,11 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    # Third Party
+
+    # Own
+    'products',
 ]
 
 MIDDLEWARE = [
