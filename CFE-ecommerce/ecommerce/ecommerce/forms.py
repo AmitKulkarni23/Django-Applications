5fce97ebb185065be47e40a7f8a02cdc071586b6
amitrkulkarni232@gmail.com <amitrkulkarni232@gmail.com>
2019-02-25 19:07:43 -0500

    Creating and Validating Django forms done

---
+++ CFE-ecommerce/ecommerce/ecommerce/forms.py
@@ -0,0 +1,28 @@
+from django import forms
+
+
+class ContactForm(forms.Form):
+    """
+    Class that inherits from the Django's internal forms.Form class
+    """
+    fullname = forms.CharField(widget=forms.TextInput(attrs={"class": "form-control",
+                                                             "id": "form_full_name",
+                                                             "placeholder": "Your Full Name"}))
+    email = forms.EmailField(widget=forms.EmailInput(attrs={"class": "form-control",
+                                                            "placeholder": "Your Email"}))
+    content = forms.CharField(widget=forms.Textarea(attrs={"class": "form-control",
+                                                           "placeholder": "Your message"}))
+
+    # Error handling mechanism
+    # def clean_<field_name>
+
+    def clean_email(self):
+        """
+        CError handling mechanism for email
+        :return:
+        """
+        email = self.cleaned_data.get("email")
+        if not "gmail.com" in email:
+            raise forms.ValidationError("Email has to be gamil.com")
+
+        return email
