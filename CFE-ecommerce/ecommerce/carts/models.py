de5b99365407885c4397e512eee37c7c63e41549
amitrkulkarni232@gmail.com <amitrkulkarni232@gmail.com>
2019-03-17 10:06:43 -0400

    Cart Create method inside of views

--- CFE-ecommerce/ecommerce/carts/models.py
+++ CFE-ecommerce/ecommerce/carts/models.py
@@ -1,3 +1,19 @@
 from django.db import models
+from django.conf import settings
+from products.models import Product
+
+User = settings.AUTH_USER_MODEL
+
 
 # Create your models here.
+class Cart(models.Model):
+    # Any user can create a cart( authenticated or non-authenticated)
+    user = models.ForeignKey(User, null=True, blank=True, on_delete=models.CASCADE)
+    # Have the ability to have a blank cart
+    products = models.ManyToManyField(Product, blank=True)
+    total = models.DecimalField(default=0.0, max_digits=10, decimal_places=2)
+    timestamp = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return str(self.id)
--- CFE-ecommerce/ecommerce/carts/views.py
+++ CFE-ecommerce/ecommerce/carts/views.py
@@ -1,11 +1,32 @@
 from django.shortcuts import render
+from .models import Cart
+
+
+def cart_create(user=None):
+    print("New cart created")
+    cart_obj = Cart.objects.create(user=None)
+    return cart_obj
 
 
 # Create your views here.
 def cart_home(request):
+
+    request.session["cart_id"] = "12"
+    # We dont want to create a cart if it already exists
+    cart_id = request.session.get("cart_id", None)
+
+    qs = Cart.objects.filter(id=cart_id)
+    if qs.count() == 1:
+        print("Cart exists")
+        cart_obj = qs.first()
+    else:
+        print("Create a new cart")
+        cart_obj = cart_create()
+        request.session["cart_id"] = cart_obj.id
+
+
     # key = request.session.session_key
 
-    request.session["first_name"] = "Amit"
     # Expire the session key after 300 seconds
     # request.session.set_expiry(300)
     return render(request, "carts/home.html", {})
