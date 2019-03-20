4c0f0283b9130a5be109fe4f94587c180a9448b8
amitrkulkarni232@gmail.com <amitrkulkarni232@gmail.com>
2019-03-19 21:34:59 -0400

    Order Total calcualted

--- CFE-ecommerce/ecommerce/orders/models.py
+++ CFE-ecommerce/ecommerce/orders/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 from carts.models import Cart
 from ecommerce.utils import unique_order_id_generator
-from django.db.models.signals import pre_save
+from django.db.models.signals import pre_save, post_save
 
 
 # (db _stored_value, display_value)
@@ -26,10 +26,24 @@ class Order(models.Model):
     status = models.CharField(max_length=120, default="created", choices=ORDER_STATUS_CHOICES)
 
     shipping_total = models.DecimalField(default=5.99, max_digits=100, decimal_places=2)
-    order_total = models.DecimalField(default=0.00, max_digits=100, decimal_places=2)
+    total = models.DecimalField(default=0.00, max_digits=100, decimal_places=2)
 
     def __str__(self):
         return self.order_id
+
+    # helper function
+    def update_total(self):
+        print("Calling update total")
+        cart_total = self.cart.total
+        print("CART_TOTAL = ", cart_total)
+        shipping_total = self.shipping_total
+
+        new_total = cart_total + shipping_total
+
+        self.total = new_total
+        self.save()
+
+        return new_total
 
 
 def pre_save_create_order_id(sender, instance, *args, **kwargs):
@@ -39,6 +53,34 @@ def pre_save_create_order_id(sender, instance, *args, **kwargs):
 
 pre_save.connect(pre_save_create_order_id, sender=Order)
 
+
+def post_save_cart_total(sender, instance, created, *args, **kwargs):
+    print("FROM CART TOTAL", created)
+    if not created:
+        # Just to avoid confusion
+        cart_obj = instance
+
+        cart_id = cart_obj.id
+        print("The cart id is ", cart_id)
+
+        qs = Order.objects.filter(cart__id=cart_id)
+
+        if qs.count() == 1:
+            print("Into the if loop")
+            order_obj = qs.first()
+            order_obj.update_total()
+
+
+post_save.connect(post_save_cart_total, sender=Cart)
+
+
+def post_save_order(sender, instance, created, *args, **kwargs):
+    print("FROM SAVE ORDER", created)
+    if created:
+        instance.update_total()
+
+
+post_save.connect(post_save_order, sender=Order)
 
 # Generate the order_id
 # Generate the order total
