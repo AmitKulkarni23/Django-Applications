d2de06302fa84fea1ce15fd7635974554975d4d3
amitrkulkarni232@gmail.com <amitrkulkarni232@gmail.com>
2019-09-24 02:23:33 -0400

    Template Inheritance Done

--- TryDjango2.2/trydjango/src/ballblog/ballblog/views.py
+++ TryDjango2.2/trydjango/src/ballblog/ballblog/views.py
@@ -1,5 +1,6 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+from django.template.loader import get_template
 
 
 def home_page(request):
@@ -15,4 +16,11 @@ def about_page(request):
 def contact_page(request):
     my_title = "Contact Us"
     return render(request, "contact.html", {"title": my_title})
+
+
+def example_page(request):
+    template_name = "home.html"
+    template_obj = get_template(template_name)
+    context = {"title": "Example"}
+    return HttpResponse(template_obj.render(context))
 
